ac9aebf320b2283ce9209bd8a0f87ef9aec3d6d0
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-05-03 12:14:02 +0200

    admin_page: introduced ETF_selector logic

--- admin_page.py
+++ admin_page.py
@@ -1,5 +1,22 @@
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+import pandas as pd
 import streamlit as st
 
+from src.util import get_ishares_etfs
+
+def sequential_df_filter(df, to_filter_columns):
+    for col in to_filter_columns:
+        col_pretty = col.replace("_", " ").title()
+        choices = st.multiselect(
+            f"Select {col_pretty}",
+            list(df[col].unique()))
+        df = df[df[col].isin(choices)]
+    return df
 
 if not st.user.is_logged_in:
     if st.button("Log in"):
@@ -13,6 +30,29 @@ else:
     if st.user.email in st.secrets.security.admins:
         st.write(f"Welcome, {st.user.name}!")
         # Show admin-only features here
+        st.subheader(f"ETF Selector")
+        etfs = get_ishares_etfs()
+        etfs_filt = sequential_df_filter(
+                etfs,
+                ['region', 'country', 'subasset_class'])
+        if etfs_filt.shape[0] > 0:
+            st.write("Select rows")
+            event = st.dataframe(
+                etfs_filt,
+                # column_config=column_configuration,
+                use_container_width=True,
+                hide_index=True,
+                on_select="rerun",
+                selection_mode="multi-row",
+            )
+            selected_rows = event.selection.rows
+            etfs_filt2 = etfs_filt.iloc[selected_rows]
+
+            # # Display selected ETF
+            if etfs_filt2.shape[0] > 0:
+                st.write(f"Selected ETFs:")
+                st.write(', '.join(etfs_filt2.symbol.values))
+
     else:
         st.write(f"User ({st.user.name}) not authorized")
         st.stop()
