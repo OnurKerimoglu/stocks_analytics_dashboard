085c1e6bab0df2be5d1d9808c5dcaaba3fae40bf
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-05-03 14:50:36 +0200

    admin_page: moved the etf-browser logic to browser tab, created manager tab

--- admin_page.py
+++ admin_page.py
@@ -7,7 +7,11 @@ from pandas.api.types import (
 import pandas as pd
 import streamlit as st
 
-from src.utils import get_ishares_etfs
+from src.utils import get_ishares_etfs, run_query
+from src.queries import Queries
+
+
+queries = Queries()
 
 def sequential_df_filter(df, to_filter_columns):
     for col in to_filter_columns:
@@ -30,28 +34,45 @@ else:
     if st.user.email in st.secrets.security.admins:
         st.write(f"Welcome, {st.user.name}!")
         # Show admin-only features here
-        st.subheader(f"ETF Selector")
-        etfs = get_ishares_etfs()
-        etfs_filt = sequential_df_filter(
-                etfs,
-                ['region', 'country', 'subasset_class'])
-        if etfs_filt.shape[0] > 0:
+        manager, browser = st.tabs(["Manager", "Browser"])
+
+        with manager:
+            st.subheader(f"Manage Tracked ETFs:")
+            user = st.user.name
+            user_etfs_query = queries.user_etfs(user)
+            df = run_query(user_etfs_query)
             st.write("Select rows")
             event = st.dataframe(
-                etfs_filt,
-                # column_config=column_configuration,
-                use_container_width=True,
-                hide_index=True,
-                on_select="rerun",
-                selection_mode="multi-row",
-            )
-            selected_rows = event.selection.rows
-            etfs_filt2 = etfs_filt.iloc[selected_rows]
+                    df,
+                    # column_config=column_configuration,
+                    use_container_width=True,
+                    hide_index=True,
+                    on_select="rerun",
+                    selection_mode="multi-row",
+                )
+        with browser:
+            st.subheader(f"Browse ETFs:")
+            etfs = get_ishares_etfs()
+            etfs_filt = sequential_df_filter(
+                    etfs,
+                    ['region', 'country', 'subasset_class'])
+            if etfs_filt.shape[0] > 0:
+                st.write("Select rows")
+                event = st.dataframe(
+                    etfs_filt,
+                    # column_config=column_configuration,
+                    use_container_width=True,
+                    hide_index=True,
+                    on_select="rerun",
+                    selection_mode="multi-row",
+                )
+                selected_rows = event.selection.rows
+                etfs_filt2 = etfs_filt.iloc[selected_rows]
 
-            # # Display selected ETF
-            if etfs_filt2.shape[0] > 0:
-                st.write(f"Selected ETFs:")
-                st.write(', '.join(etfs_filt2.symbol.values))
+                # # Display selected ETF
+                if etfs_filt2.shape[0] > 0:
+                    st.write(f"Selected ETFs:")
+                    st.write(', '.join(etfs_filt2.symbol.values))
 
     else:
         st.write(f"User ({st.user.name}) not authorized")
