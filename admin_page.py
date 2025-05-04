d5a51f3d09df59c0d8b7d3cf67c27d2659e9d9a5
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-05-04 12:07:27 +0200

    admin_page: added the functionalities to add to and remove from the ETFs table

--- admin_page.py
+++ admin_page.py
@@ -1,3 +1,4 @@
+from google.oauth2 import service_account
 from pandas.api.types import (
     is_categorical_dtype,
     is_datetime64_any_dtype,
@@ -7,10 +8,11 @@ from pandas.api.types import (
 import pandas as pd
 import streamlit as st
 
-from src.utils import get_ishares_etfs, run_query
+from src.utils import get_ishares_etfs, run_query_nocache, execute_query, insert_df_to_table
 from src.queries import Queries
 
-
+USER_DATASET = "stocks_user_data"
+ETFS_TABLE = "ETFS_to_track"
 queries = Queries()
 
 def sequential_df_filter(df, to_filter_columns):
@@ -22,12 +24,6 @@ def sequential_df_filter(df, to_filter_columns):
         df = df[df[col].isin(choices)]
     return df
 
-# Function to update selection when multiselect is changed
-def update_selection():
-    print(f'final_list: {st.session_state.final_list}')
-    st.session_state.selection = st.session_state.final_list.copy()
-    print(f'selection: {st.session_state.selection}')
-
 if not st.user.is_logged_in:
     if st.button("Log in"):
         st.login()
@@ -35,22 +31,34 @@ if not st.user.is_logged_in:
 else:
     if st.button("Log out"):
         st.logout()
+    username = st.user.name
+    useremail = st.user.email
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account_admin"])
 
     # Check for admin status based on email domain
     if st.user.email in st.secrets.security.admins:
-        st.write(f"Welcome, {st.user.name}!")
+        st.write(f"Welcome, {username}!")
+        
         # Show admin-only features here
         etfs = get_ishares_etfs()
 
+        user_etfs_query = queries.user_etfs(
+                useremail,
+                table=f'{USER_DATASET}.{ETFS_TABLE}')
+        df = run_query_nocache(
+            user_etfs_query,
+            _credentials=credentials)
+        
+        symbols_tracked = df.symbol.unique()
+        
         manager, browser = st.tabs(["Manager", "Browser"])
-
+        
         with manager:
             st.subheader(f"Manage Tracked ETFs:")
-            user = st.user.name
-            user_etfs_query = queries.user_etfs(user)
-            df = run_query(user_etfs_query)
             if df.shape[0] > 0:
-                st.write(f"Showing currently tracked ETFs by user: {st.user.name}\nSelect rows to remove:")
+                st.write(f"Showing currently tracked ETFs by user: {useremail}.")
+                st.write("Select rows to remove:")
                 event = st.dataframe(
                         df,
                         # column_config=column_configuration,
@@ -60,14 +68,17 @@ else:
                         selection_mode="multi-row",
                     )
                 selected_rows = event.selection.rows
-                symbols = df['symbol'].iloc[selected_rows]
-                symbols_str = ', '.join(symbols)
-                confirmed = st.button(f"Confirm removing etfs: {symbols_str}")
-                if confirmed:
-                    st.write(f"Removing etfs: {symbols_str}")
-                    # todo
+                symbols_to_remove = df['symbol'].iloc[selected_rows].values
+                # print(f'symbols: {symbols}, len={len(symbols)}')
+                if len(symbols_to_remove) > 0:
+                    symbols = [f"'{symbol}'" for symbol in symbols_to_remove]
+                    symbols_str = ', '.join(symbols)
+                    confirmed = st.button(f"Confirm removing etfs: {symbols_str}")
+                    if confirmed:
+                        query = f"DELETE FROM stocks_user_data.ETFS_to_track WHERE symbol IN ({symbols_str}) AND user = '{useremail}'"
+                        execute_query(query, credentials)
             else:
-                st.write(f"Currently no tracked ETFs by user: {st.user.name}")
+                st.write(f"Currently no tracked ETFs by user: {useremail}")
             
             st.write("Add new ETFs:")
             # Initialize session state variables
@@ -82,16 +93,19 @@ else:
             user_input = user_input.upper().strip()
             # Button to add item
             if st.button("Add to List"):
-                if user_input in etfs.symbol.values:
-                    if user_input not in st.session_state.selection:
-                        st.session_state.selection.append(user_input)
-                        st.session_state.final_list = st.session_state.selection.copy()  # Force update
-                        st.success(f"'{user_input}' added to the list.")
+                if user_input not in symbols_tracked:
+                    if user_input in etfs.symbol.values:
+                        if user_input not in st.session_state.selection:
+                            st.session_state.selection.append(user_input)
+                            st.session_state.final_list = st.session_state.selection.copy()  # Force update
+                            st.success(f"'{user_input}' added to the list.")
+                        else:
+                            st.info(f"'{user_input}' is already in the list.")
                     else:
-                        st.info(f"'{user_input}' is already in the list.")
+                        st.warning(f"'{user_input}' is not available")
+                        st.write("Enter one of: {}".format(', '.join(etfs.symbol.values)))
                 else:
-                    st.warning(f"'{user_input}' is not available")
-                    st.write("Enter one of: {}".format(', '.join(etfs.symbol.values)))
+                    st.info(f"'{user_input}' is already tracked.")
 
             # Show current list
             if st.session_state.final_list:
@@ -110,7 +124,16 @@ else:
             if st.session_state.confirmed:
                 list_str = ', '.join(st.session_state.final_list)
                 st.info(f"ETFs to be added: {list_str}")
-                # todo
+                # create dataframe based on values:
+                df = pd.DataFrame({
+                    'symbol': st.session_state.final_list,
+                    'user': useremail})
+                # print(df.head())
+                # adding to BQ table
+                Query = insert_df_to_table(
+                    df=df,
+                    dataset_table=f'{USER_DATASET}.{ETFS_TABLE}',
+                    credentials=credentials)
             
         with browser:
             st.subheader(f"Browse ETFs:")
@@ -136,5 +159,5 @@ else:
                     st.write(', '.join(etfs_filt2.symbol.values))
 
     else:
-        st.write(f"User ({st.user.name}) not authorized")
+        st.write(f"User {username} ({useremail}) not authorized")
         st.stop()
